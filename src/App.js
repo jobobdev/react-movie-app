5e3663c7a3853aaf3fa9e339b40f0b078d5a0e7f
jobobdev <tmvle22@gmail.com>
2022-12-29 14:00:18 +0900

    Lec #6.4 Cleanup Function

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (e) => setKeyword(e.target.value);
+  const [showing, setShowing] = useState(false);
+  function Hello() {
+    useEffect(() => {
+      console.log("Created :)");
+      return () => console.log("Destroyed :(");
+    }, []);
+    return <h1>Hello</h1>;
+  }
 
-  useEffect(() => {
-    console.log("i run only once");
-  }, []); // empty array => 리액트가 변화를 감지할 것이 없으므로 한번만 실행하게 됨.
-  useEffect(() => {
-    console.log("i run when 'keyword' changes");
-  }, [keyword]); // === if keyword changes, run this code
-  useEffect(() => {
-    console.log("i run when 'counter' changes");
-  }, [count]);
-  useEffect(() => {
-    console.log("i run when 'keyword' & 'counter' changes");
-  }, [keyword, count]);
-
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{count}</h1>
-      <button onClick={onClick}>Click Me!</button>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
@@ -48,3 +34,6 @@ export default App;
 // click me 버튼을 누를 때도 마찬가지로 re-rendering이 된다.
 // 그렇게 되면, 검색을 하고 나서 click을 누를 때마다 검색이 새로 되는 말도 안되는 상황이 발생한다.
 // 그 때 useEffect의 두번 째 인자를 사용한다. [] 안에 update되는지를 감지할 변수를 넣는다.
+
+// useEffect를 컴포넌트를 create할 때 사용한다면,
+// react는 컴포넌트를 destroy할 때에도 함수를 실행 하게 해준다.
